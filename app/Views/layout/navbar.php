e9a85a80909209a7691df78c3c2f74995a21db0c
xyrez27 <aldior@upnvj.ac.id>
2022-07-15 08:46:36 +0700

    new update

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,16 +1,25 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
-        <a class="navbar-brand" href="/" alt="Angkasa Pura II">BA Angkasa Pura II</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="/">
+            <img src="/img/ap2.jpg" alt="" class="logo-ap2">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link" href="/">BA Pemeriksa Pekerjaan</a>
-                <a class="nav-link" href="/">BA Pembayaran</a>
-                <a class="nav-link" href="/">Formulir</a>
-                <a class="nav-link" href="/">Surat Pernyataan</a>
-            </div>
+        <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
+                        Berita Acara
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Surat Pemeriksaan</a>
+                        <a class="dropdown-item" href="#">Surat Permohonan Pembayaran</a>
+                        <a class="dropdown-item" href="#">Formulir</a>
+                        <a class="dropdown-item" href="#">Surat Pernyataan</a>
+                    </div>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
