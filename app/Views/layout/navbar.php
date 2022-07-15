3f87a790e483ef846126d58b209f3f1a725a130d
xyrez27 <aldior@upnvj.ac.id>
2022-07-15 13:44:03 +0700

    navbar updated

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -3,24 +3,30 @@
         <a class="navbar-brand" href="/">
             <img src="/img/ap2.jpg" alt="" class="logo-ap2">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
-            <ul class="navbar-nav">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
-                        <b>Berita Acara</b>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="/pages/ba">BA</a>
-                        <a class="dropdown-item" href="#">Surat Pemeriksaan</a>
-                        <a class="dropdown-item" href="#">Surat Permohonan Pembayaran</a>
-                        <a class="dropdown-item" href="#">Formulir</a>
-                        <a class="dropdown-item" href="#">Surat Pernyataan</a>
-                    </div>
-                </li>
-            </ul>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link" href="/pages/ba">Berita Acara</a>
+            </div>
         </div>
     </div>
 </nav>
+
+<!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="/" alt="Angkasa Pura II">Angkasa Pura II</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" href="/">Home</a>
+                <a class="nav-link" href="/pages/about">About us</a>
+                <a class="nav-link" href="/pages/contact">Contact</a>
+                <a class="nav-link" href="/passengers">Passengers</a>
+            </div>
+        </div>
+    </div>
+</nav> -->
