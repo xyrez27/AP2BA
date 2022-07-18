2702baca64513cdf5cf146e1874daf2394e7fa3f
xyrez27 <aldior@upnvj.ac.id>
2022-07-18 15:10:10 +0700

    UPDATE INDEX

--- app/Views/ba/index.php
+++ app/Views/ba/index.php
@@ -14,34 +14,57 @@
                         </div>
                     </div>
                 </div>
-                <div class="row mb-3">
-                    <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="alamat" name="alamat" value="<?= old('alamat'); ?>">
-                    </div>
-                </div>
-                <section>
-                    <div class="row form-group">
-                        <label for="date" class="col-sm-1 col-form-label">Date</label>
-                        <div class="col-sm-4">
-                            <div class="input-group date" id="datepicker">
-                                <input type="text" class="form-control">
-                                <span class="input-group-append">
-                                    <span class="input-group-text bg-white">
-                                        <i class="fa fa-calendar"></i>
-                                    </span>
+                <div class="row form-group">
+                    <label for="date" class="col-2 col-form-label">Date</label>
+                    <div class="col">
+                        <div class="input-group date" id="datepicker">
+                            <input type="text" class="form-control">
+                            <span class="input-group-append">
+                                <span class="input-group-text bg-white">
+                                    <i class="fa fa-calendar"></i>
                                 </span>
-                            </div>
+                            </span>
                         </div>
                     </div>
-                </section>
-                <script type="text/javascript">
-                    $(function() {
-                        $('datepicker').datepicker();
-                    });
-                </script>
+                </div>
+                <!-- <div class="row mb-3">
+                    <label for="alamat" class="col-sm-2 col-form-label">M.A No</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="MA_No" name="MA_No" value="">
+                    </div>
+                </div> -->
+                <div class="row mb-3">
+                    <div class="col-10">
+                        <label for="MA_No" class="col-2 col-form-label">M.A No</label>
+                        <select id="MA_No" class="form-select">
+                            <option>Eksploitasi</option>
+                            <option>Investasi</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="nama" class="col-sm-2 col-form-label">RKA Tahun</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="RKA" name="RKA" autofocus value="">
+                        <div id="validationServer04Feedback" class="invalid-feedback">
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="nama" class="col-sm-2 col-form-label">Lampiran</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="lampiran" name="lampiran" autofocus value="">
+                        <div id="validationServer04Feedback" class="invalid-feedback">
+                        </div>
+                    </div>
+                </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
+            <!-- <script type="text/javascript">
+                $(function() {
+                    $('datepicker').datepicker();
+                });
+            </script> -->
         </div>
     </div>
 </div>
