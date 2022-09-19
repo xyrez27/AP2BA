8740b8cbbe92b4b8ca96ed6fc084f2ddd29b3044
xyrez27 <aldior@upnvj.ac.id>
2022-09-19 18:52:50 +0700

    push gaming

--- app/Views/viewforpdf/suratpernyataan_pdf.php
+++ app/Views/viewforpdf/suratpernyataan_pdf.php
@@ -6,143 +6,48 @@
             <div class="row mt-4">
                 <div class="col">
                     <div class="page1">
-                        <h2 style="text-align: center;"><b><u>SURAT PERNYATAAN PEMBAYARAN</u></b></h2>
+                        <h2 style="text-align: center;"><b><u>SURAT PERNYATAAN PEMBAYARAN</u></b></h2><br>
                         <!-- Under Table -->
-                        <?php $i = 1; ?>
-                        <table style="border-style: none;">
-                            <?php foreach ($karyawanap2 as $kp) : ?>
-                                <tr>
-                                    <td style="border-style: none;"><?= $i; ?>. </td>
-                                    <td style="border-style: none;">Nama : <?= $kp; ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                                    <td style="border-style: none;">Jabatan : <?= $jabatanap2[$i - 1]; ?>;</td>
-                                </tr>
-                                <?php $i++ ?>
-                            <?php endforeach; ?><br>
-                        </table>
-                        <p><b>PT ANGKASA PURA SOLUSI :</b></p>
-                        <?php $j = 1; ?>
-                        <table style="border-style: none;">
-                            <?php foreach ($karyawanaps as $ks) : ?>
-                                <tr>
-                                    <td style="border-style: none;"><?= $j; ?>. </td>
-                                    <td style="border-style: none;">Nama : <?= $ks; ?> &nbsp;&nbsp;&nbsp;&nbsp;</td>
-                                    <td style="border-style: none;">Jabatan : <?= $jabatanaps[$j - 1]; ?>;</td>
-                                </tr>
-                                <?php $j++ ?>
-                            <?php endforeach; ?><br>
-                        </table>
-                        <p>Berdasarkan :</p>
-                        <p>1. Perjanjian Sewa Menyewa Nomor PJJ.06.02/01/<?= date('m/Y', strtotime($sewapc['tanggal_ba'])); ?>/<?= $sewapc['no_psm']; ?> tanggal <?= date('d m Y', strtotime($sewapc['tanggal_psm'])); ?>
-                            tentang <?= $sewapc['judul_ba']; ?>;
+                        <p>
+                            Kami yang bertandatangan di bawah ini, menyatakan dengan sesungguhnya : <br>
                         </p>
-                        <p>2. Berita Acara Operasional Nomor BAC.06.02/01/<?= date('m/Y', strtotime($sewapc['tanggal_ba'])); ?>/<?= $sewapc['no_bao']; ?> tanggal <?= date('d m Y', strtotime($sewapc['tanggal_bao'])); ?>
-                            tentang <?= $sewapc['judul_ba']; ?>;
-                        </p><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-                        <p style="text-align: right;">Telah………../2</p>
-                    </div>
-                    <div class="page2">
-                        <p>Telah melakukan pemeriksaan pekerjaan dan menyatakan bahwa Pekerjaan <?= $sewapc['judul_ba']; ?>
-                            untuk periode pemakaian <?= date('d m Y', strtotime($sewapc['tanggal_pp_from'])); ?> s.d. <?= date('d m Y', strtotime($sewapc['tanggal_pp_to'])); ?>
-                            telah terlaksana dengan baik sesuai ketentuan dengan rincian perangkat terpasang sebagai berikut :
-                        </p>
-                        <table style="margin-left: auto; margin-right: auto; width: 90%;">
-                            <thead>
-                                <tr style="background-color: #e6e6e6;">
-                                    <th>NO</th>
-                                    <th>JENIS KOMPUTER</th>
-                                    <th>JUMLAH</th>
-                                </tr>
-                            </thead>
-                            <tbody style="text-align: center;">
-                                <?php $k = 1; ?>
-                                <?php foreach ($jenis_komputer as $jk) : ?>
-                                    <tr>
-                                        <td><?= $k; ?></td>
-                                        <td style="text-align: left;"><?= $jk; ?></td>
-                                        <td><?= $unit_komputer[$k - 1]; ?> Unit</td>
-                                    </tr>
-                                    <?php $k++ ?>
-                                <?php endforeach; ?>
-                                <tr>
-                                    <th colspan="2">TOTAL</th>
-                                    <td><?= $sewapc['jumlah_unit']; ?> Unit</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <p>Demikian Berita Acara Pemeriksaan Pekerjaan ini dibuat dalam rangkap 2 (dua) untuk dapat digunakan sebagaimana mestinya.</p>
                         <table style="border-style: none;">
-                            <thead>
-                                <tr>
-                                    <th colspan="7" style="text-align: left; border-style: none;">PT ANGKASA PURA SOLUSI</th>
-                                    <th style="border-style: none;">&nbsp;&nbsp;&nbsp;&nbsp;</th>
-                                    <th colspan="7" style="text-align: left; border-style: none;">PT ANGKASA PURA II</th>
-                                </tr>
-                                <tr>
-                                    <th colspan="7" style="border-style: none;"><br><br></th>
-                                    <th style="border-style: none;"></th>
-                                    <th colspan="7" style="border-style: none;"></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td style="border-style: none;">1.</td>
-                                    <td style="border-style: none;" colspan="4"><u><?= strtoupper($karyawanaps[0]); ?></u></td> <!-- Karyawan APS -->
-                                    <td style="border-style: none;"></td>
-                                    <td style="border-style: none;">……………</td>
-                                    <td style="border-style: none;"></td>
-                                    <td style="border-style: none;">1.</td>
-                                    <td style="border-style: none;" colspan="4"><u><?= strtoupper($karyawanap2[1]); ?></u></td> <!-- Karyawan AP2 -->
-                                    <td style="border-style: none;"></td>
-                                    <td style="border-style: none;">……………</td>
-                                </tr>
-                                <tr>
-                                    <td style="border-style: none;"></td>
-                                    <td style="border-style: none;" colspan="6"><?= $jabatanaps[0]; ?></td> <!-- Jabatan APS -->
-                                    <td style="border-style: none;"></td>
-                                    <td style="border-style: none;"></td>
-                                    <td style="border-style: none;" colspan="6"><?= $jabatanap2[1]; ?></td> <!-- Jabatan AP2 -->
-                                </tr>
-                                <tr>
-                                    <td style="border-style: none;" colspan="7"><br><br></td>
-                                    <td style="border-style: none;"></td>
-                                    <td style="border-style: none;" colspan="7"></td>
-                                </tr>
-                                <tr>
-                                    <td style="border-style: none;">2.</td>
-                                    <td style="border-style: none;" colspan="4"><u><?= strtoupper($karyawanaps[1]); ?></u></td> <!-- Karyawan APS -->
-                                    <td style="border-style: none;"></td>
-                                    <td style="border-style: none;">……………</td>
-                                    <td style="border-style: none;"></td>
-                                    <td style="border-style: none;">2.</td>
-                                    <td style="border-style: none;" colspan="4"><u><?= strtoupper($karyawanap2[2]); ?></u></td> <!-- Karyawan AP2 -->
-                                    <td style="border-style: none;"></td>
-                                    <td style="border-style: none;">……………</td>
-                                </tr>
-                                <tr>
-                                    <td style="border-style: none;"></td>
-                                    <td style="border-style: none;" colspan="6"><?= $jabatanaps[1]; ?></td> <!-- Jabatan APS -->
-                                    <td style="border-style: none;"></td>
-                                    <td style="border-style: none;"></td>
-                                    <td style="border-style: none;" colspan="6"><?= $jabatanap2[2]; ?></td> <!-- Jabatan AP2 -->
-                                </tr>
-                            </tbody>
-                        </table><br /><br /><br /><br /><br /><br />
-                        <table style="margin-left: auto; margin-right: auto; width: 100%; border-style: none;">
+                            <tr style="text-align: left; vertical-align: top;">
+                                <td style="border-style: none;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1.&nbsp;&nbsp;&nbsp;</td>
+                                <td style="text-align: justify; border-style: none;">Bahwa telah dilaksanakan proses pemeriksaan pekerjaan <?= $sewapc['judul_ba']; ?>;</td>
+                            </tr>
+                            <tr style="text-align: left; vertical-align: top;">
+                                <td style="border-style: none;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2.&nbsp;&nbsp;&nbsp;</td>
+                                <td style="text-align: justify; border-style: none;">Kami telah memeriksa, meneliti dan melakukan tindakan-tindakan yang diperlukan dalam rangka pembayaran pekerjaan <?= $sewapc['judul_ba']; ?></td>
+                            </tr>
+                            <tr style="text-align: left; vertical-align: top;">
+                                <td style="border-style: none;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;3.&nbsp;&nbsp;&nbsp;</td>
+                                <td style="text-align: justify; border-style: none;"></td>
+                            </tr>
+                            <tr style="text-align: left; vertical-align: top;">
+                                <td style="border-style: none;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4.&nbsp;&nbsp;&nbsp;</td>
+                                <td style="text-align: justify; border-style: none;">Sehubungan dengan hal-hal tersebut diatas, kami sanggup untuk mempertanggungjawabkan proses pembayaran dimaksud.</td>
+                            </tr>
+                        </table>
+                        <p>Demikian pernyataan ini kami sampaikan dengan sebenar-benarnya tanpa menyembunyikan fakta-fakta dan hal-hal material apapun, dengan demikian kami akan bertanggung jawab sepenuhnya atas kebenaran dari hal-hal yang kami nyatakan disini, sesuai dengan fungsi tugas dan tanggung jawab jabatan kami.</p>
+                        <br><br>
+                        <p style="text-align: right;">Tangerang, &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Juli 2022 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><br>
+                        <table style="width: 90%; margin-left: auto; margin-right: auto; border-style: none;">
                             <tbody>
                                 <tr style="text-align: center;">
-                                    <td style="border-style: none;">Mengetahui :</td>
+                                    <td style="width: 33%; border-style: none;"><?= $jabatanap2[0]; ?></td>
+                                    <td style="width: 30%; border-style: none;"></td>
+                                    <td style="width: 35%; border-style: none;"><?= $jabatanap2[1]; ?></td>
                                 </tr>
                                 <tr style="text-align: center;">
-                                    <td style="border-style: none;">PT ANGKASA PURA II</td>
+                                    <td style="border-style: none;"><br><br>Materai<br>(10.000)<br><br><br></td>
+                                    <td style="border-style: none;"></td>
+                                    <td style="border-style: none;"></td>
                                 </tr>
                                 <tr style="text-align: center;">
-                                    <td style="border-style: none;"><br><br><br><br><br></td>
-                                </tr>
-                                <tr style="text-align: center;">
-                                    <td style="border-style: none;"><u><?= strtoupper($karyawanap2[0]); ?></u></td>
-                                </tr>
-                                <tr style="text-align: center;">
-                                    <td style="border-style: none;"><?= strtoupper($jabatanap2[0]); ?></td>
+                                    <td style="border-style: none;"><?= $karyawanap2[0]; ?></td>
+                                    <td style="border-style: none;"></td>
+                                    <td style="border-style: none;"><?= $karyawanap2[1]; ?></td>
                                 </tr>
                             </tbody>
                         </table>
