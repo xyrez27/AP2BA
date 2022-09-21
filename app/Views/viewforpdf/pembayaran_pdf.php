964422536da937d32688635544e22196eee46d1a
greeel11 <albaniktsr11@gmail.com>
2022-09-21 09:49:50 +0700

    Update pembayaran

--- app/Views/viewforpdf/pembayaran_pdf.php
+++ app/Views/viewforpdf/pembayaran_pdf.php
@@ -60,6 +60,34 @@
                                 <td colspan="2" style="line-height: 1.8; padding-left: 15px; border-style: none;">bertindak untuk dan atas nama PT Angkasa Pura Solusi, untuk selanjutnya disebut <b>PIHAK KEDUA.</b><br><br></td>
                             </tr>
                         </table>
+                        <p>Berdasarkan :</p>
+                        <p>1. Perjanjian Sewa Menyewa Nomor PJJ.06.02/01/<?= date('m/Y', strtotime($sewapc['tanggal_ba'])); ?>/<?= $sewapc['no_psm']; ?> tanggal <?= date('d m Y', strtotime($sewapc['tanggal_psm'])); ?>
+                            tentang <?= $sewapc['judul_ba']; ?>;</p>
+                        <p>2. Berita Acara Kesepakatan Peneyesuaian Tarif Pajak Pertambahan Nilai (PPN) BAC.06.02/01/<?= $sewapc['judul_ba']; ?> Nomor : BAC.06.02/01/<?= $sewapc['no_ppn']; ?> tanggal <?= date('d m Y', strtotime($sewapc['tanggal_ppn'])); ?>
+                            tentang <?= $sewapc['judul_ba']; ?>;</p>
+                        <p>3. Berita Acara Pemeriksaan Pekerjaan BAC.06.02/01/<?= date('m/Y', strtotime($sewapc['tanggal_ba'])); ?>/<?= $sewapc['no_pemeriksaan']; ?> tanggal <?= date('d m Y', strtotime($sewapc['tanggal_ba'])); ?>
+                            tentang <?= $sewapc['judul_ba']; ?>;</p>
+                        <p>Maka pada PIHAK KEDUA dapat dilakukan pembayaran tahap <?= $sewapc['tahap_ke']; ?> untuk periode <?= $sewapc['tanggal_pp_from']; ?> s.d. <?= $sewapc['tanggal_pp_to']; ?></p><br /><br />
+                        <p style="text-align: right;">Pembayaran………../2</p>
+                        <p>Pembayaran dilakukan dengan rincian sebagai berikut : </p>
+                        <p>Tahap <?= $sewapc['tahap_ke']; ?> (Periode <?= $sewapc['tanggal_pp_from']; ?> s.d <?= $sewapc['tanggal_pp_to']; ?>)</p>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th colspan="5">DETAIL PEMBAYARAN TAHAP <?= $sewapc['tahap_ke']; ?></th>
+                                </tr>
+                                <tr>
+                                    <th>NO</th>
+                                    <th>ITEM</th>
+                                    <th>QUANTITY</th>
+                                    <th>HARGA SATUAN</th>
+                                    <th>TOTAL</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr></tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
