eacd1eda516ffefb57f0d6ec1f352de9c5122cbb
xyrez27 <aldior@upnvj.ac.id>
2022-09-21 21:11:20 +0700

    s

--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -5,29 +5,20 @@
     <div class="container">
         <div class="row py-4">
             <div class="col">
-                <div class="card" style="width: 18rem;">
+                <div class="card" style="width: 100%;">
                     <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="card-title"><b>BERITA ACARA SEWA PC</b></h5><br><br>
+                        <p class="card-text">Berita Acara Sewa PC terdiri dari form pemeriksaan, pembayaran, formulir, dan surat pernyataan</p>
+                        <a href="/ba/sewa-pc" class="btn btn-primary">Isi Form</a>
                     </div>
                 </div>
             </div>
             <div class="col">
-                <div class="card text-center" style="width: 18rem;">
+                <div class="card" style="width: 100%;">
                     <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card text-right" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="card-title"><b>DAFTAR BERITA ACARA SEWA PC</b></h5><br><br>
+                        <p class="card-text">Berisi daftar-daftar berita acara sewa pc yang telah diinput berupa form pemeriksaan, pembayaran, formulir, dan surat pernyataan</p>
+                        <a href="/ba/daftarBA" class="btn btn-primary">Lihat Daftar BA</a>
                     </div>
                 </div>
             </div>
