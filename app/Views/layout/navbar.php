129c728c4d3bc512cea52f7bf9b39b56b12b8b27
xyrez27 <aldior@upnvj.ac.id>
2022-09-21 21:45:39 +0700

    ss

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
-        <a class="navbar-brand" href="/">
+        <a class="navbar-brand" href="/pages/dashboard">
             <img src="/img/ap2.jpg" alt="" class="logo-ap2">
         </a>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="/pages/dashboard" role="button" data-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
                         Berita Acara
                     </a>
                     <div class="dropdown-menu">
